57ac654c349491e38fc5532c206d65f44d891eb3
dhruvil <dhruvil1514@gmail.com>
2019-07-15 15:38:06 +0530

    adding the changes

---
+++ src/api-functions/stories.js
@@ -0,0 +1,37 @@
+import calculateTimeDifferance from './time';
+ import getSource from './getSource';
+var number = 0;
+async function fetchLiveStories(count){
+try{
+var response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+var allTheStories = await response.json();
+
+
+const jsonData = allTheStories.slice(count,count+30).map( (element)=>{
+    return  fetch(`https://hacker-news.firebaseio.com/v0/item/${element}.json?print=pretty`).
+    then(response => response.json()).then(data=>{
+     return {
+          'by':data.by, 
+          'score':data.score,
+          'time' : calculateTimeDifferance(data.time),
+          'url' : data.url,
+          'source': getSource(data.url),
+          'title': data.title,
+          'comments':data.descendants 
+        } 
+      }) ;
+})
+count = count+30;
+
+return jsonData
+// var liveStories = await Promise.all(jsonData);
+// console.log(liveStories);
+}
+catch(err){
+   console.log(err); 
+} 
+}
+
+
+// fetchLiveStories(number);
+export default fetchLiveStories

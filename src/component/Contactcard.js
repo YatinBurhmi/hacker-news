aff955a75675c2d4f46739f4340091b159d2616f
vivek mehta <vivekmehta9162@gmail.com>
2019-07-20 06:21:50 +0530

    fix layout issues

--- src/component/Contactcard.js
+++ src/component/Contactcard.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Button, Card } from "react-bootstrap";
 
-function CreateCard(props){
+function CreateCard(props) {
   var level = props.contact.level;
-  var indent = (level)*40;
-  var indentForCard = `${indent}px`
-  return(
-      <div style={{
-        position:"relative",
-        left:indentForCard,
-        width:"70%"
-      }}>
-      <h1>{props.contact.by}</h1>
-      <span>{props.contact.time}</span>
-      <p>{props.contact.text}</p>
-      <button onClick={()=>props.function(props.contact.object,props.contact.level)}>load 2nd level </button>
+  var indent = level * 50;
+  var indentForCard = `${indent}px`;
+  console.log(props)
+  return (
+    <Card >
+      <div style={{ position: "relative", left: indentForCard, width: 900 }}>
+        <b
+          style={{
+            fontFamily: "Helvetica Neue,Helvetica,Arial,sans-serif",
+            color: "#b92c27"
+          }}
+        >
+          {props.contact.by}
+        </b>
+        <br />
+        <span style={{ fontSize: "x-small" }}>{props.contact.time}</span>
+        <br />
+        <b style={{ fontSize: "small" }}>{props.contact.text}</b>
+        <br />
+        <Button
+          variant="Light"
+          style={{ outlineColor: "none" }}
+          onClick={() =>
+            props.function(props.contact.object, props.contact.level)
+          }
+        >
+          More Comment
+        </Button>
       </div>
-  )
+    </Card>
+  );
 }
 
-export default CreateCard
+export default CreateCard;

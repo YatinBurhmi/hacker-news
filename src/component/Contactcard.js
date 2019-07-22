ada788c90c0aa9035723884bb58d914e998d86e6
vivek mehta <vivekmehta9162@gmail.com>
2019-07-22 15:21:15 +0530

    loading screen

--- src/component/Contactcard.js
+++ src/component/Contactcard.js
@@ -1,36 +1,38 @@
 import React, { Fragment } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function CreateCard(props) {
   var level = props.contact.level;
   var indent = level * 50;
   var indentForCard = `${indent}px`;
+  var commentText = props.contact.text;
   return (
     <Fragment>
-      <p />
-      <Card style={{backgroundColor:"#dadddf"}}>
-        <div style={{ position: "relative", left: indentForCard, width: 900 }}>
-          <img
-            src={require("../images/images.png")}
-            style={{ height: 40, width: 40, borderRadius: 50 }}
-            alt=""
-          />{" "}
-          <b
-            style={{
-              fontFamily: "Helvetica Neue,Helvetica,Arial,sans-serif",
-              color: "#b92c27"
-            }}
-          >
+      <div
+        className="media mb-3"
+        style={{ position: "relative", left: indentForCard, width: 1000 }}
+      >
+        <div className="media-body p-2 shadow-sm rounded bg-light border">
+          <small className="float-right text-muted">{props.contact.time}</small>
+          <h6 className="mt-0 mb-1">
+            <img
+              className="mr-3 bg-light rounded"
+              width="48"
+              height="48"
+              src={`https://api.adorable.io/avatars/48/${
+                props.contact.by
+              }@adorable.io.png`}
+              alt={props.contact.by}
+            />
             {props.contact.by}
-          </b>
-          <br />
-          <span style={{ fontSize: "x-small" }}>{props.contact.time}</span>
-          <br />
-          <b style={{ fontSize: "small" }}>{props.contact.text}</b>
-          <br />
+          </h6>
+          <p
+            dangerouslySetInnerHTML={{ __html: commentText }}
+            style={{ fontSize: "small" }}
+          />
           <Button
-            variant="Light"
-            style={{ outlineColor: "none" }}
+            variant="info"
+            size="sm"
             onClick={() =>
               props.function(props.contact.object, props.contact.level)
             }
@@ -38,7 +40,7 @@ function CreateCard(props) {
             View Reply
           </Button>
         </div>
-      </Card>
+      </div>
     </Fragment>
   );
 }
--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,103 +1,138 @@
 import React, { Component } from "react";
 import fetchLiveStories from "../api-functions/stories";
-import { Card, Button, Media, Badge } from "react-bootstrap";
+import { Card, Button, Media, Badge, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "../css/loadingIcon.css";
 var number = 0;
 
 export class home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoaded: false,
       liveStories: [],
-      numberOfStories:0
+      numberOfStories: 0
     };
     this.updateStories = this.updateStories.bind(this);
+    // this.seachItem = this.seachItem.bind(this);
   }
 
   async componentDidMount() {
     var allThePromisies = fetchLiveStories(number);
     var liveStories = await allThePromisies;
     this.setState({
+      isLoaded: true,
       liveStories: liveStories
     });
   }
 
-  async updateStories(){
+  async updateStories() {
     var number = this.state.numberOfStories;
-    number = number+30;
+    number = number + 30;
     var allThePromisies = fetchLiveStories(number);
     var liveStories = await allThePromisies;
     this.setState({
-       liveStories: liveStories,
-       numberOfStories: number
-     });
-   } 
+      liveStories: liveStories,
+      numberOfStories: number
+    });
+  }
+
+  defaultSrc = ev => {
+    ev.target.src = "https://gitlab.com/favicon.ico";
+  };
 
   render() {
-    return (
-      <div>
-        {this.state.liveStories.map(story => (
-          <Card
-            key={story.id}
-            style={{ width: "inherit", height: "7rem", padding: 20, backgroundColor:"#fdf5e2" }}
+    if (!this.state.isLoaded) {
+      return (
+        <div className="loader">
+          <Spinner animation="grow" variant="secondary" />
+          <Spinner animation="grow" variant="success" />
+          <Spinner animation="grow" variant="danger" />
+          <Spinner animation="grow" variant="warning" />
+          <Spinner animation="grow" variant="info" />
+          <Spinner animation="grow" variant="light" />
+          <Spinner animation="grow" variant="dark" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {this.state.liveStories
+            .filter(e => e.title.toLowerCase())
+            .map(story => (
+              <Card
+                key={story.id}
+                style={{
+                  width: "inherit",
+                  padding: 20,
+                  backgroundColor: "#fdf5e2"
+                }}
+              >
+                <Media>
+                  <img
+                    width={64}
+                    height={64}
+                    className="mr-3"
+                    src={`http://${story.source}/favicon.ico`}
+                    alt="not found"
+                    onError={this.defaultSrc}
+                  />
+                  <Media.Body>
+                    <a href={story.url} target="blank">
+                      <b
+                        style={{
+                          marginLeft: -347,
+                          color: "black",
+                          fontSize: "large"
+                        }}
+                      >
+                        {story.title}
+                      </b>
+                    </a>
+                    <span>-({story.source})</span>
+                    <br />
+                    <span>By - {story.by}</span>
+                    <br />
+                    <span style={{ fontSize: "small" }}>
+                      <b>{story.score}-Points</b>{" "}
+                      <b style={{ paddingLeft: 10 }}>{story.time}</b>
+                    </span>
+                    <span>
+                      <Link
+                        style={{ marginLeft: -320, color: "blue" }}
+                        to={{
+                          pathname: "/comments",
+                          state: { comment: story.object }
+                        }}
+                      >
+                      <span><i class="fa fa-comments-o" style={{fontSize:30,paddingTop:10}}/></span>
+                      <span>{story.comments}</span>
+                      </Link>
+                    </span>
+                    <span>
+                      <a
+                        href={`http://www.twitter.com/share?url=${story.url}`}
+                        target="blank"
+                      >
+                        <Badge style={{ marginLeft: -330 }} variant="info">
+                          Tweet
+                        </Badge>
+                      </a>
+                    </span>
+                  </Media.Body>
+                </Media>
+              </Card>
+            ))}
+          <Button
+            onClick={this.updateStories}
+            variant="danger"
+            style={{ width: 1110 }}
           >
-            <Media>
-              <img
-                width={64}
-                height={64}
-                className="mr-3"
-                src={`http://${story.source}/favicon.ico`}
-                alt=""
-              />
-              <Media.Body>
-                <a href={story.url} target="blank">
-                  <b
-                    style={{
-                      marginLeft: -347,
-                      color: "black",
-                      fontSize: "large"
-                    }}
-                  >
-                    {story.title}
-                  </b>
-                </a>
-                <span>-({story.source})</span>
-                <br />
-                <span>By - {story.by}</span>
-                <br />
-                <span style={{fontSize:"small"}}>
-                  <b>{story.score}-Points</b>{" "}<b style={{paddingLeft:10}}>{story.time}</b>
-                </span>
-                <span>
-                  <Link
-                    style={{ marginLeft: -320, color: "orange" }}
-                    to={{
-                      pathname: "/comments",
-                     state: { comment:story.object }
-                    }}
-                  >
-                    {story.comments}-Comments
-                  </Link>
-                </span>
-                <span>
-                  <a
-                    href={`http://www.twitter.com/share?url=${story.url}`}
-                    target="blank"
-                  >
-                    <Badge style={{ marginLeft: -330 }} variant="info">
-                      Tweet
-                    </Badge>
-                  </a>
-                </span>
-              </Media.Body>
-            </Media>
-          </Card>
-        ))}
-        <Button onClick={this.updateStories} variant="danger" style={{ width: 1110 }}>
-          + MORE NEWS
-        </Button>
-      </div>
-    );
+            + More News
+          </Button>
+        </div>
+      );
+    }
   }
 }
 
